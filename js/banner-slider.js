4958529a74cbbcea8f0daed4a389db5184fbfaec
Alexader Rostiahai <rostyagai.dev@gmail.com>
2024-08-05 20:45:11 +0300

    Add banner slider component

---
+++ js/banner-slider.js
@@ -0,0 +1,27 @@
+const bannerSlider = new Swiper(".banner-slider", {
+  speed: 600,
+  navigation: {
+    nextEl: "#banner-slider-next-btn",
+    prevEl: "#banner-slider-prev-btn",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.4,
+      spaceBetween: 12,
+      slidesPerGroup: 1,
+      initialSlide: 2,
+    },
+    764: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      slidesPerGroup: 2,
+      initialSlide: 3,
+    },
+    1280:{
+      spaceBetween: 30,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      initialSlide: 3,
+    }
+  },
+});
