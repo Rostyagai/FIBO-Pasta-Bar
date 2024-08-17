3e4b9ccccb477d3a26b0e56f03d9599ab927d43b
Alexader Rostiahai <rostyagai.dev@gmail.com>
2024-08-17 17:41:44 +0300

    fix new products reveal

--- js/scroll-animations.js
+++ js/scroll-animations.js
@@ -1,3 +1,3 @@
 ScrollReveal().reveal(".banner-section", { delay: 500});
 ScrollReveal().reveal(".products-gallery__item", { delay: 300, distance: "-60px" });
-ScrollReveal().reveal(".new-products__item", { delay: 500, distance: "100%", origin: 'right' });
+ScrollReveal().reveal(".new-products__list", { delay: 500, distance: "10%", origin: 'right' });
