f1b79ae5b374ccc399ab2480809f464702dc6a50
Alexader Rostiahai <rostyagai.dev@gmail.com>
2024-08-21 13:03:25 +0300

    Fix css selector for scroll reveal

--- js/scroll-animations.js
+++ js/scroll-animations.js
@@ -1,4 +1,4 @@
 ScrollReveal().reveal("section.load-hidden", { delay: 500});
-ScrollReveal().reveal("background-illustration.load-hidden", { delay: 500});
+ScrollReveal().reveal(".background-illustration.load-hidden", { delay: 500});
 ScrollReveal().reveal(".products-gallery__item", { delay: 300, distance: "-60px" });
 ScrollReveal().reveal(".new-products__list", { delay: 500, distance: "10%", origin: 'right' });
