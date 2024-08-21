d4547cfae894bf69165e54504b056c73e1e5dfa7
Alexader Rostiahai <rostyagai.dev@gmail.com>
2024-08-21 12:48:32 +0300

    Add scroll loading to all sections

--- js/scroll-animations.js
+++ js/scroll-animations.js
@@ -1,3 +1,3 @@
-ScrollReveal().reveal(".banner-section", { delay: 500});
+ScrollReveal().reveal("section.load-hidden", { delay: 500});
 ScrollReveal().reveal(".products-gallery__item", { delay: 300, distance: "-60px" });
 ScrollReveal().reveal(".new-products__list", { delay: 500, distance: "10%", origin: 'right' });
